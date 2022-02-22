6651fc308cd238e9f0fcb8f6d729b401e6ddbaa1
jakobfitzke <99833223+jakobfitzke@users.noreply.github.com>
2022-02-22 19:42:58 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Paddle from "/src/paddle";
+import InputHandler from "/src/input";
+import Ball from "/src/ball";
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball();
+
+paddle.draw(ctx);
+
+new InputHandler(paddle);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+  ball.draw(ctx);
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
